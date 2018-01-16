89f521d546e50b6dc7ad7b6d7daaa64b21d800c3
ethan375 <ethanebel1@gmail.com>
2018-01-15 22:56:25 -0600

    Got stuck on saturday and started over, canvas created

---
+++ js/app.js
@@ -0,0 +1,5 @@
+console.log("JS file linked");
+// grab the canvas by the id
+let canvas = document.getElementById('myCanvas');
+// canvas context will be a 2d game
+let ctx = canvas.getContext('2d');
