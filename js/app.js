896737e6f198dc4176eca381bdc570aa718bbe1a
ethan375 <ethanebel1@gmail.com>
2018-01-17 03:06:37 -0600

    broke enemies and lasers, totally stuck

--- js/app.js
+++ js/app.js
@@ -6,43 +6,23 @@ let ctx = canvas.getContext('2d');
 
 // global variables
 let playerX = 350; //this is the starting x coordinate for the player 
-let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
+//let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
 let laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
-let FPS = 60; //this is our frames per second
+let laserX = playerX;
+// let bullets = [];
+// let aliens = [];
+
+// window.onload =()=>{
+//   drawAliens();
+//   player();
+//   laser();
+// }
 
 let drawAliens = ()=>{
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(30,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(70,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(110,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(150,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(190,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(230,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(270,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(310,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(350,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(390,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(430,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(470,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(510,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(550,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(590,30,30,20)
-  ctx.fillStyle = 'rgb(20, 200, 30)';
-  ctx.fillRect(630,30,30,20)
+  for(let i =0; i<10;i++){
+    ctx.fillStyle = 'rgb(20, 200, 30)';
+    ctx.fillRect(Math.floor((Math.random()*700)+5), 30, 30, 20)
+  }
 }
 
 let player = ()=>{
@@ -54,9 +34,12 @@ let laser = ()=>{
   ctx.fillStyle = 'rgb(200, 30, 50)'
   ctx.fillRect(laserX + 10 , laserY, 10, 20);
   laserY -= 10
-  if(laserY < 60){
-    laserReset();
-  }
+  setInterval(laser, 500)
+  
+  // if(laserY < 60){
+  //   laserReset();
+  // }
+  // taking above code out, will get back around to this functonality when I get to CD
 };
 
 
@@ -67,14 +50,14 @@ document.addEventListener('keydown', function(event){
   }else if(key == 37){
     playerX = playerX - 10;
   }else if(key == 32){
-   Laser();
+    setInterval(laser, 500)  
   }
   // console.log(key)
   // console.log(playerX)
 });
 
 let laserReset = ()=>{
-   laserX = laserX;
+   laserX += 10;
    laserY = 650
 }
 
@@ -84,14 +67,13 @@ let laserReset = ()=>{
 
 let animateCanvas = ()=>{
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawAliens();
   player();
-  laser();
+  drawAliens();
   window.requestAnimationFrame(animateCanvas);
 
 };
-
 animateCanvas();
+
 
 
 
