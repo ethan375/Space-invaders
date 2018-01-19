ea6af7512f50035fb73a8680a3a8c9c32f1273b4
ethan375 <ethanebel1@gmail.com>
2018-01-19 00:39:31 -0600

    beginning to figure out rows and cols

--- js/app.js
+++ js/app.js
@@ -5,19 +5,18 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 // // global variables
-// let playerX = 350; //this is the starting x coordinate for the player 
-// //let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
-// const laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
-// const laserX = playerX;
-// let bullets = [];
+let playerX = 350; //this is the starting x coordinate for the player 
+let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
+let laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
+let laserArray = [];
 
 
-
-function Aliens (x, y, dx, dy){
+//constructor function for the aliens
+function Aliens (x, y, ){
   this.x = x;
   this.y = y;
-  this.dx = dx;
-  this.dy = dy;
+  // this.dx = dx;
+  // this.dy = dy;
 
   this.draw = ()=>{
     ctx.fillStyle = 'rgb(20, 230, 30)'
@@ -27,14 +26,14 @@ function Aliens (x, y, dx, dy){
 
 
   this.update = ()=>{
-    if(this.x + 5 > canvas.Width || this.x -5 < 0){
-      this.dx = -dx
-    }
-    if(this.x + 5 > canvas.width || this.x - 5 < 0){
-      this.dy = -dy;
-    }
-    this.x += dx;
-    this.y -= dy;
+    // if(this.x  > 700 || this.x < 0){
+    //   this.dx = -dx
+    // }
+    // if(this.x + 5 > 700 || this.x - 5 < 0){
+    //   this.dy = -dy;
+    // }
+    // this.x += dx;
+    // this.y -= dy;
     this.draw();
   }
 }
@@ -42,11 +41,17 @@ function Aliens (x, y, dx, dy){
   const alienArray = [];
 
 for(let i = 0; i < 10; i++){
-  let x = Math.random() * canvas.height;
-  let y = 100;
-  let dx = 0;
-  let dy = 0;
-  alienArray.push(new Aliens(x, y, dx, dy))
+  let x = 15;
+  let y = 10;
+  let row = 6;
+  let col = 7;
+  for(let i=0; i < row;i++){
+    for(let j=0; j<col; j++){
+      alienArray.push(new Aliens(x, y))
+      x += 20;
+    }
+    y += 30;
+  }
 }
 
 
@@ -55,33 +60,34 @@ for(let i = 0; i < 10; i++){
 //   ctx.fillRect(100,100,10,20)
 // }
 
-// let player = ()=>{
-//   ctx.fillStyle = 'rgb(20, 20, 200)'
-//   ctx.fillRect(playerX, 670, 30, 20)
-// }
+let player = ()=>{
+  ctx.fillStyle = 'rgb(20, 20, 200)'
+  ctx.fillRect(playerX, 670, 30, 20)
+}
 
-// let laser = ()=>{
-//   ctx.fillStyle = 'rgb(200, 30, 50)'
-//   ctx.fillRect(laserX + 10 , laserY, 10, 20);
+// function laser (y, dy) {
+//   this.y = y;
+//   this.dy = dy;
+
+//   thi
+//   ctx.fillStyle = 'rgb(200, 30, 50)';
+//   ctx.fillRect(laserX + 10 , 650, 10, 20);
 //   laserY -= 10
-
 // };
 
 
-// document.addEventListener('keydown', function(event){
-//   let key = event.which;
-//   if(key == 39){
-//     playerX = playerX + 10;
-//   }else if(key == 37){
-//     playerX = playerX - 10;
-//   }else if(key == 32){ 
-//   }
-// });
+document.addEventListener('keydown', function(event){
+  let key = event.which;
+  if(key == 39){
+    playerX = playerX + 10;
+  }else if(key == 37){
+    playerX = playerX - 10;
+  }else if(key == 32){ 
+    // laserArray.push(new laser)
+  }
+});
 
-// let laserReset = ()=>{
-//    laserX += 10;
-//    laserY = 650
-// }
+
 
 
 
@@ -93,7 +99,7 @@ let animateCanvas = ()=>{
   for (let i=0; i < 10; i++) {
     alienArray[i].update();
   }
-  // player();
+  player();
 };
 animateCanvas();
 
