213e687e6d3db99cba9542ef5251ef62a34869de
ethan375 <ethanebel1@gmail.com>
2018-01-19 03:27:28 -0600

    Can only get one line of enemies and having trouble getting the laser function to work correctly

--- js/app.js
+++ js/app.js
@@ -5,10 +5,19 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 // // global variables
-let playerX = 350; //this is the starting x coordinate for the player 
-let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
-let laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
-let laserArray = [];
+const playerX = 350; //this is the starting x coordinate for the player 
+
+
+const laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
+
+
+const laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
+
+//the array that will be populated when a laser is fired by the user. It will be depopulated when the laser either hits the top of the screen or one of the aliens
+const laserArray = [];
+
+//the array that will be populated when the game loads with all of the avliable aliens
+const alienArray = [];
 
 
 //constructor function for the aliens
@@ -20,7 +29,7 @@ function Aliens (x, y, ){
 
   this.draw = ()=>{
     ctx.fillStyle = 'rgb(20, 230, 30)'
-    ctx.fillRect(this.x, this.y, 10, 20);
+    ctx.fillRect(this.x, this.y, 30, 20);
   },
 
 
@@ -38,19 +47,25 @@ function Aliens (x, y, ){
   }
 }
 
-  const alienArray = [];
 
 for(let i = 0; i < 10; i++){
-  let x = 15;
-  let y = 10;
-  let row = 6;
-  let col = 7;
+  x = 100;
+  y = 10;
+  let row = 5;
+  let col = 10;
+  //this loop is going to create the rows
   for(let i=0; i < row;i++){
+    //this loops will create the columns within the rows
     for(let j=0; j<col; j++){
+      //here we instantiate the aliens constructor 
       alienArray.push(new Aliens(x, y))
-      x += 20;
+       // after every new alien in the col we are moving the next one to the right
+      x += 50;
     }
+    //this should bring our next row down 30 pixsels
     y += 30;
+        //this starts every new column in its original starting place
+    x = 100
   }
 }
 
@@ -65,15 +80,46 @@ let player = ()=>{
   ctx.fillRect(playerX, 670, 30, 20)
 }
 
-// function laser (y, dy) {
+
+// function laser (x, y, dy) {
+//   this.x = playerX + 10;
 //   this.y = y;
 //   this.dy = dy;
+//   this.update();
 
-//   thi
-//   ctx.fillStyle = 'rgb(200, 30, 50)';
-//   ctx.fillRect(laserX + 10 , 650, 10, 20);
-//   laserY -= 10
+//   this.draw = ()=>{
+//     ctx.fillStyle = 'rgb(200, 30, 50)';
+//     ctx.fillRect(this.x, this.y, 10, 20);
+//   }
+
+//   this.update=()=>{
+//   //starting coordinate
+//   let y = 670;
+//   //the velocity that the laser is going to travel
+//   let dy = 10;
+//   y -= dy
+//     this.draw();
+//   }
+
+
 // };
+
+function Laser (x, y, dy){
+  this.x = laserX +10;
+  this.y = laserY;
+  this.dy = dy;
+
+
+  draw = ()=>{
+    ctx.fillStyle = 'rgb(200, 30, 50)';
+    ctx.fillRect(this.y, this.x, 10, 20)
+  }
+
+  update =()=>{
+
+  }
+
+}
 
 
 document.addEventListener('keydown', function(event){
@@ -102,7 +148,6 @@ let animateCanvas = ()=>{
   player();
 };
 animateCanvas();
-
 
 
 
