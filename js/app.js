93df2db277ecb697d455fcaf247711794c009466
ethan375 <ethanebel1@gmail.com>
2018-01-18 22:55:18 -0600

    invaders are showing up on canvas at with random x values

--- js/app.js
+++ js/app.js
@@ -4,78 +4,104 @@ const canvas = document.getElementById('myCanvas');
 // canvas context will be a 2d game
 const ctx = canvas.getContext('2d');
 
-// global variables
-let playerX = 350; //this is the starting x coordinate for the player 
-//let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
-const laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
-const laserX = playerX;
+// // global variables
+// let playerX = 350; //this is the starting x coordinate for the player 
+// //let laserX = playerX //this will be the position of the laser when fired, being equal to player's x coordinate will make it appear as if it is coming from the players ship
+// const laserY = 650; //this will be y coordinate for the bullet which will travel upwards on y axis when shot
+// const laserX = playerX;
 // let bullets = [];
-let alienX = 80;
-const alienY = 70;
-// const aliens = [10];
 
 
-let aliens = (x, y)=>{
+
+function Aliens (x, y, dx, dy){
   this.x = x;
   this.y = y;
-}
+  this.dx = dx;
+  this.dy = dy;
+
+  this.draw = ()=>{
+    ctx.fillStyle = 'rgb(20, 230, 30)'
+    ctx.fillRect(this.x, this.y, 10, 20);
+  },
 
 
-let player = ()=>{
-  ctx.fillStyle = 'rgb(20, 20, 200)'
-  ctx.fillRect(playerX, 670, 30, 20)
-}
 
-let laser = ()=>{
-  ctx.fillStyle = 'rgb(200, 30, 50)'
-  ctx.fillRect(laserX + 10 , laserY, 10, 20);
-  laserY -= 10
-  setInterval(laser, 500)
-  
-  // if(laserY < 60){
-  //   laserReset();
-  // }
-  // taking above code out, will get back around to this functonality when I get to CD
-};
-
-
-document.addEventListener('keydown', function(event){
-  let key = event.which;
-  if(key == 39){
-    playerX = playerX + 10;
-  }else if(key == 37){
-    playerX = playerX - 10;
-  }else if(key == 32){ 
+  this.update = ()=>{
+    if(this.x + 5 > canvas.Width || this.x -5 < 0){
+      this.dx = -dx
+    }
+    if(this.x + 5 > canvas.width || this.x - 5 < 0){
+      this.dy = -dy;
+    }
+    this.x += dx;
+    this.y -= dy;
+    this.draw();
   }
-});
+}
 
-let laserReset = ()=>{
-   laserX += 10;
-   laserY = 650
+  const alienArray = [];
+
+for(let i = 0; i < 10; i++){
+  let x = Math.random() * canvas.height;
+  let y = 100;
+  let dx = 0;
+  let dy = 0;
+  alienArray.push(new Aliens(x, y, dx, dy))
 }
 
 
+// let how = ()=>{
+//   ctx.fillStyle = 'rgb(20,230,30)';
+//   ctx.fillRect(100,100,10,20)
+// }
 
+// let player = ()=>{
+//   ctx.fillStyle = 'rgb(20, 20, 200)'
+//   ctx.fillRect(playerX, 670, 30, 20)
+// }
 
+// let laser = ()=>{
+//   ctx.fillStyle = 'rgb(200, 30, 50)'
+//   ctx.fillRect(laserX + 10 , laserY, 10, 20);
+//   laserY -= 10
 
-// let animateCanvas = ()=>{
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   player();
-//   animateAliens();
-//   window.requestAnimationFrame(animateCanvas);
 // };
-// animateCanvas();
+
+
+// document.addEventListener('keydown', function(event){
+//   let key = event.which;
+//   if(key == 39){
+//     playerX = playerX + 10;
+//   }else if(key == 37){
+//     playerX = playerX - 10;
+//   }else if(key == 32){ 
+//   }
+// });
+
+// let laserReset = ()=>{
+//    laserX += 10;
+//    laserY = 650
+// }
+
+
+
+
+
+let animateCanvas = ()=>{
+  window.requestAnimationFrame(animateCanvas);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i=0; i < 10; i++) {
+    alienArray[i].update();
+  }
+  // player();
+};
+animateCanvas();
 
 
 
 
 
 
-  // let count = 16;
-  // let x = 30
-  // let y = 30
-  // for(let i = 0; i<count; i++){
-  //   ctx.fillStyle = 'rgb(20, 200, 30)';
-  //   ctx.fillRect(x + 50,y,30,20)
-  // }
+
+
 
